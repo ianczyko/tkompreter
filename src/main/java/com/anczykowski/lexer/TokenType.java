30253a9f7000cb43421f41c0d2c0a622378f1d0d
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2023-04-06 19:27:47 +0200

    Removed arity property

--- src/main/java/com/anczykowski/lexer/TokenType.java
+++ src/main/java/com/anczykowski/lexer/TokenType.java
@@ -1,8 +1,5 @@
 package com.anczykowski.lexer;
 
-import lombok.Getter;
-
-
 public enum TokenType {
     COMMENT,
     IDENTIFIER,
@@ -34,15 +31,5 @@ public enum TokenType {
     LT, GT, NEG,
 
     // 2-character operators
-    LE(2), GE(2), NE(2), EQ(2), ARROW(2);
-
-    @Getter
-    private int arity = 1;
-
-    TokenType() {
-    }
-
-    TokenType(int arity) {
-        this.arity = arity;
-    }
+    LE, GE, NE, EQ, ARROW;
 }

a7135f78d4d26329e51b7185d5c1688c610c7e44
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2023-05-27 21:40:21 +0200

    unused import

--- src/main/java/com/anczykowski/interpreter/value/ClassValue.java
+++ src/main/java/com/anczykowski/interpreter/value/ClassValue.java
@@ -3,7 +3,6 @@ package com.anczykowski.interpreter.value;
 import com.anczykowski.interpreter.Context;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
